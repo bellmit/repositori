367a793597e4f7bfc600ae5e0c3b434a462130e6
degloba Projects <degloba@degloba.com>
2020-10-28 20:29:59 +0100

    r

---
+++ ecommerce/ecommerce-application/src/main/java/com/degloba/ecommerce/vendes/application/services/ICarroCompraService.java
@@ -0,0 +1,12 @@
+package com.degloba.ecommerce.vendes.application.services;
+
+
+
+/**
+ * Shopping Cart is just an Application Feature, with no Domain Model
+ * 
+ */
+public interface ICarroCompraService {
+	
+	public void afegir(long producteId);
+}
