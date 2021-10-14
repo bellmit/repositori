63edb78865dfa83e5d4fc43a0f39eada2304788e
degloba <degloba@degloba.com>
2021-10-14 18:56:54 +0200

    e

---
+++ ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/enviaments/infrastructure/persistence/nosql/mongo/EnviamentReactiveRepository.java
@@ -0,0 +1,32 @@
+package com.degloba.ecommerce.enviaments.infrastructure.persistence.nosql.mongo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.degloba.ecommerce.enviaments.domain.persistence.nosql.mongo.Enviament;
+import com.degloba.ecommerce.enviaments.domain.persistence.nosql.mongo.IEnviamentReactiveRepository;
+import com.degloba.infrastructure.services.GenericRepositoryImpl;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+/////public interface IEnviamentReactiveRepository extends ReactiveMongoRepository<Enviament, String> {
+
+
+@Repository
+public class EnviamentReactiveRepository extends GenericRepositoryImpl<Enviament, String> implements IEnviamentReactiveRepository {
+	
+
+
+}
