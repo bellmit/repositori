4607aab9ea139e9e3f6f45fb4553e8d99ec95643
degloba <degloba@degloba.com>
2021-10-22 19:32:14 +0200

    r

--- ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/crm/infrastructure/persistence/rdbms/api/jpa/repositories/CrmRepository.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/crm/infrastructure/persistence/rdbms/api/jpa/repositories/CrmRepository.java
@@ -1,7 +1,20 @@
 package com.degloba.ecommerce.crm.infrastructure.persistence.rdbms.api.jpa.repositories;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import com.degloba.domain.annotations.DomainRepositoryImpl;
+import com.degloba.ecommerce.crm.domain.persistence.rdbms.jpa.Client;
 import com.degloba.ecommerce.crm.domain.persistence.rdbms.jpa.ICrmRepository;
+
+import com.degloba.infrastructure.services.GenericJPARepositoryImpl;
+import com.degloba.infrastructure.services.GenericRepositoryImpl;
 import com.degloba.persistence.rdbms.api.jpa.EntityRepository;
 
 
@@ -11,8 +24,10 @@ import com.degloba.persistence.rdbms.api.jpa.EntityRepository;
  * @author degloba
  *
  */
-@DomainRepositoryImpl
-public class CrmRepository extends EntityRepository implements ICrmRepository{
+////@DomainRepositoryImpl
+////public class CrmRepository extends EntityRepository implements ICrmRepository{
+	
+public class CrmRepository extends GenericJPARepositoryImpl<Client, String> implements ICrmRepository{
 
-
+	
 }
