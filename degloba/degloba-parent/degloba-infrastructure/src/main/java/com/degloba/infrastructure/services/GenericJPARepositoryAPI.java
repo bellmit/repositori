41eca3ce9c583edb31017322db573f88cc26bcf6
degloba <degloba@degloba.com>
2021-10-22 19:29:00 +0200

    r

--- degloba/degloba-parent/degloba-infrastructure/src/main/java/com/degloba/infrastructure/services/GenericJPARepositoryAPI.java
+++ degloba/degloba-parent/degloba-infrastructure/src/main/java/com/degloba/infrastructure/services/GenericJPARepositoryAPI.java
@@ -2,9 +2,11 @@ package com.degloba.infrastructure.services;
 
 import java.io.Serializable;
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 
@@ -14,7 +16,7 @@ public interface GenericJPARepositoryAPI<T, ID extends Serializable> {
 
 	List<T> findAll(Sort sort);
 
-	List<T> findAllById(Iterable<ID> ids);
+	List<T> findAllById(Iterable<String> ids);
 
 	<S extends T> List<S> saveAll(Iterable<S> entities);
 
@@ -22,27 +24,50 @@ public interface GenericJPARepositoryAPI<T, ID extends Serializable> {
 
 	<S extends T> S saveAndFlush(S entity);
 
-	<S extends T> List<S> saveAllAndFlush(Iterable<S> entities);
-
-	@Deprecated
-	default void deleteInBatch(Iterable<T> entities){deleteAllInBatch(entities);}
-
 	void deleteAllInBatch(Iterable<T> entities);
 
+	<S extends T> List<S> saveAllAndFlush(Iterable<S> entities);
 
-	void deleteAllByIdInBatch(Iterable<ID> ids);
 
 	void deleteAllInBatch();
 
-	@Deprecated
-	T getOne(ID id);
+	void deleteAllByIdInBatch(Iterable<String> ids);
 
-	T getById(ID id);
+	T getOne(String id);
 
+	T getById(String id);
 
 	<S extends T> List<S> findAll(Example<S> example);
 
+	Page<T> findAll(Pageable pageable);
+
+	<S extends T> S save(S entity);
+
+	Optional<T> findById(String id);
+
 	<S extends T> List<S> findAll(Example<S> example, Sort sort);
+
+	boolean existsById(String id);
+
+	long count();
+
+	void deleteById(String id);
+
+	void deleteAllById(Iterable<? extends String> ids);
+
+	void delete(T entity);
+
+	void deleteAll(Iterable<? extends T> entities);
+
+	void deleteAll();
+
+	<S extends T> Optional<S> findOne(Example<S> example);
+
+	<S extends T> Page<S> findAll(Example<S> example, Pageable pageable);
+
+	<S extends T> long count(Example<S> example);
+
+	<S extends T> boolean exists(Example<S> example);
 
 		
 }
--- degloba/degloba-parent/degloba-infrastructure/src/main/java/com/degloba/infrastructure/services/GenericJPARepositoryImpl.java
+++ degloba/degloba-parent/degloba-infrastructure/src/main/java/com/degloba/infrastructure/services/GenericJPARepositoryImpl.java
@@ -1,13 +1,186 @@
 package com.degloba.infrastructure.services;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
-
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public abstract class GenericJPARepositoryImpl<T, ID extends Serializable> implements GenericJPARepositoryAPI<T, ID> {
 
-	
+	@Override
+	public List<T> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<T> findAll(Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<T> findAllById(Iterable<String> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends T> List<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends T> S saveAndFlush(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends T> List<S> saveAllAndFlush(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAllInBatch(Iterable<T> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllByIdInBatch(Iterable<String> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T getOne(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends T> List<S> findAll(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends T> List<S> findAll(Example<S> example, Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<T> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends T> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<T> findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(T entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllById(Iterable<? extends String> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends T> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends T> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends T> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends T> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends T> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 
 }
