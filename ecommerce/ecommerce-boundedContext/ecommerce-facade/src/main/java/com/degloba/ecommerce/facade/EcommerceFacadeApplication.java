57edf3c509e790df84823988d80d4121fd6724f0
degloba <degloba@degloba.com>
2021-10-29 19:04:40 +0200

    r

---
+++ ecommerce/ecommerce-boundedContext/ecommerce-facade/src/main/java/com/degloba/ecommerce/facade/EcommerceFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.degloba.ecommerce.facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFacadeApplication.class, args);
+	}
+
+}
---
+++ ecommerce/ecommerce-boundedContext/ecommerce-facade/src/test/java/com/degloba/ecommerce/facade/EcommerceFacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degloba.ecommerce.facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
