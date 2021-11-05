d81457137e7705c215610f5cd53da20e1de9c881
degloba <degloba@degloba.com>
2021-11-05 18:54:44 +0100

    r

--- ecommerce/ecommerce-boundedContext/ecommerce-web-reactive/src/main/java/com/degloba/ecommerce/enviaments/webreactive/EnviamentSpringApplication.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-web-reactive/src/main/java/com/degloba/ecommerce/enviaments/webreactive/EnviamentSpringApplication.java
@@ -1,4 +1,4 @@
-package com.degloba.ecommerce.enviaments.webapp.reactive;
+package com.degloba.ecommerce.enviaments.webreactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
