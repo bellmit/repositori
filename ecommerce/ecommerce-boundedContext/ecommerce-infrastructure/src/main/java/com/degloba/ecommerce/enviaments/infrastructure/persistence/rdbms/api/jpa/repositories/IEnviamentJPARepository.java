f4ad70e6190f5f8f8fc7d23dfb5ba4cca62cf268
degloba <degloba@degloba.com>
2021-11-05 19:09:36 +0100

    r

--- ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/enviaments/infrastructure/persistence/rdbms/api/jpa/repositories/IEnviamentJPARepository.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/enviaments/infrastructure/persistence/rdbms/api/jpa/repositories/IEnviamentJPARepository.java
@@ -3,8 +3,8 @@ package com.degloba.ecommerce.enviaments.infrastructure.persistence.rdbms.api.jp
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+import com.degloba.domain.persistence.nosql.GenericRepositoryAPI;
 import com.degloba.ecommerce.enviaments.domain.persistence.rdbms.jpa.Enviament;
-import com.degloba.infrastructure.services.GenericRepositoryAPI;
 
 
 public interface IEnviamentJPARepository extends JpaRepository<Enviament, String> {
