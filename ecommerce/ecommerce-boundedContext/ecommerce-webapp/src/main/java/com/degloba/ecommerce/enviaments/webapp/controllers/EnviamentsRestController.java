fe1bbed37e20bbd6b93bba3d8f29ffec6e7196a9
degloba <degloba@degloba.com>
2021-11-12 16:39:59 +0100

    r

--- ecommerce/ecommerce-boundedContext/ecommerce-webapp/src/main/java/com/degloba/ecommerce/enviaments/webapp/controllers/EnviamentsRestController.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-webapp/src/main/java/com/degloba/ecommerce/enviaments/webapp/controllers/EnviamentsRestController.java
@@ -1,15 +1,20 @@
 package com.degloba.ecommerce.enviaments.webapp.controllers;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway;
+import org.axonframework.extensions.reactor.queryhandling.gateway.ReactorQueryGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 ////import org.apache.log4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,11 +28,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.degloba.ecommerce.enviaments.application.IEnviamentService;
-import com.degloba.ecommerce.enviaments.cqrs.commands.GuardarEnviamentCommand;
+import com.degloba.ecommerce.enviaments.cqrs.commands.CreaEnviamentCommand;
+import com.degloba.ecommerce.enviaments.cqrs.commands.GuardaEnviamentCommand;
 import com.degloba.ecommerce.enviaments.cqrs.finders.IEnviamentFinder;
-import com.degloba.ecommerce.enviaments.cqrs.queries.GetEnviamentQuery;
-
-import com.degloba.ecommerce.enviaments.domain.persistence.nosql.mongo.Enviament;
+import com.degloba.ecommerce.enviaments.cqrs.queries.ObtenirEnviamentQuery;
+import com.degloba.ecommerce.enviaments.cqrs.queries.ObtenirEnviamentsQuery;
+import com.degloba.ecommerce.enviaments.domain.entitats.Enviament;
 import com.degloba.ecommerce.enviaments.domain.persistence.nosql.mongo.EnviamentTemplateOperations;
 import com.degloba.ecommerce.enviaments.domain.persistence.nosql.mongo.IEnviamentReactiveRepository;
 import com.degloba.ecommerce.enviaments.facade.dtos.EnviamentDto;
@@ -39,7 +45,6 @@ import reactor.core.publisher.Mono;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import com.degloba.ecommerce.facade.impl.assembler.EnviamentConverter;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8887") // port intern (No Docker)
@@ -47,23 +52,22 @@ import com.degloba.ecommerce.facade.impl.assembler.EnviamentConverter;
 @RequestMapping("/enviaments")
 public class EnviamentsRestController {
 
-	@Autowired
-	IEnviamentService enviamentService;
 
-	@Autowired
-	IEnviamentFinder enviamentFinder;
-	
 	/**
 	 * Axon
 	 */
 	private CommandGateway commandGateway;
 	private QueryGateway queryGateway;
 	
+	// https://docs.axoniq.io/reference-guide/extensions/reactor/reactive-gateways
+	private ReactorQueryGateway reactiveQueryGateway;
+	private ReactorCommandGateway reactiveCommandGateway; 
+	
+	
 	@GetMapping()
 	// @ResponseStatus(HttpStatus.OK)
 	public Mono<EnviamentDto> getEnviament(@RequestParam(required = false) String queryParam) {
-
-		return Mono.fromFuture(queryGateway.query(new GetEnviamentQuery("xxxxx"), EnviamentDto.class));
+		return reactiveQueryGateway.query(new ObtenirEnviamentsQuery(), EnviamentDto.class);
 	}
 	
 
@@ -71,13 +75,62 @@ public class EnviamentsRestController {
 	// @ResponseStatus(HttpStatus.OK)
 	public Flux<EnviamentDto> getEnviaments(@RequestParam(required = false) String queryParam) {
 ////			log.debug("Received request at getExample:" + queryParam);
-		return enviamentFinder.buscaEnviaments();
+				
+		// (1)
+		return reactiveQueryGateway.scatterGather(new ObtenirEnviamentsQuery(), 
+				ResponseTypes.instanceOf(EnviamentDto.class), Duration.ofSeconds(5)).take(3);
+		
+		
+		// (2)
+		//return reactiveQueryGateway.subscriptionQuery("criteriaQuery", EnviamentDto.class);
+		
+		// (22)  . (22) es equivalent a (2)
+//		return reactiveQueryGateway.subscriptionQuery(new ObtenirEnviamentsQuery(), 
+//				ResponseTypes.instanceOf(EnviamentDto.class), 
+//				ResponseTypes.instanceOf(EnviamentDto.class))
+//                .flatMapMany(result -> result.initialResult()
+//                                             .concatWith(result.updates())
+//                                             .doFinally(signal -> result.close()));
+		
+		// (3)
+		//return reactiveQueryGateway.subscriptionQueryMany(new ObtenirEnviamentsQuery(), EnviamentDto.class);
+		
+		// (33)   . (33) es equivalent a (3)
+//		return reactiveQueryGateway.subscriptionQuery(new ObtenirEnviamentsQuery(),
+//                ResponseTypes.multipleInstancesOf(EnviamentDto.class),
+//                ResponseTypes.instanceOf(EnviamentDto.class))
+//					.flatMapMany(result -> result.initialResult()
+//                          .flatMapMany(Flux::fromIterable)
+//                          .concatWith(result.updates())
+//                          .doFinally(signal -> result.close()));
+		
+		// (4)
+		//return reactiveQueryGateway.queryUpdates("criteriaQuery", EnviamentDto.class);
+		
+		// (44)     . (44) es equivalent a (4)
+//		return reactiveQueryGateway.subscriptionQuery(new ObtenirEnviamentsQuery(), Void.class, EnviamentDto.class)
+//                .flatMapMany(result -> result.updates()
+//                                             .doFinally(signal -> result.close()));
+		
+		
 	}
+	
+   
+	
+	@PostMapping
+    public Mono<Void> sendCommand(@RequestBody EnviamentDto e) {
+		CreaEnviamentCommand command = new CreaEnviamentCommand("","","");
+        return reactiveCommandGateway.send(command);
+    }
+	
 
 	@PostMapping("add")
 	@ResponseStatus(HttpStatus.CREATED)
 	public CompletableFuture<Object> postEnviament(@RequestBody EnviamentDto e) {
-		return commandGateway.send(new GuardarEnviamentCommand(e));	
+		return commandGateway.send(new GuardaEnviamentCommand(e,"","",""));	
 	}
+	
+	
+	
 
 }
