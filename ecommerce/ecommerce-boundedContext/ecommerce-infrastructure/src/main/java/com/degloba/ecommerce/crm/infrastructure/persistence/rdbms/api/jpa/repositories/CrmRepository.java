bee52d7f8e001f49c47b33735f41165947cc5d78
degloba <degloba@degloba.com>
2021-11-30 19:59:40 +0100

    r

--- ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/crm/infrastructure/persistence/rdbms/api/jpa/repositories/CrmRepository.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/crm/infrastructure/persistence/rdbms/api/jpa/repositories/CrmRepository.java
@@ -3,11 +3,16 @@ package com.degloba.ecommerce.crm.infrastructure.persistence.rdbms.api.jpa.repos
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+
+import javax.persistence.EntityManager;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 
 import com.degloba.domain.annotations.DomainRepositoryImpl;
 import com.degloba.ecommerce.crm.domain.persistence.rdbms.jpa.Client;
@@ -27,7 +32,14 @@ import com.degloba.persistence.rdbms.api.jpa.EntityRepository;
 ////@DomainRepositoryImpl
 ////public class CrmRepository extends EntityRepository implements ICrmRepository{
 	
-public class CrmRepository extends GenericJPARepositoryImpl<Client, String> implements ICrmRepository{
+public class CrmRepository extends SimpleJpaRepository<Client, String> implements ICrmRepository{
+
+	public CrmRepository(Class<Client> domainClass, EntityManager em) {
+		super(domainClass, em);
+		// TODO Auto-generated constructor stub
+	}
+
+
 
 	
 }
--- ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/enviaments/infrastructure/persistence/nosql/mongo/EnviamentReactiveRepository.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/enviaments/infrastructure/persistence/nosql/mongo/EnviamentReactiveRepository.java
@@ -1,29 +1,54 @@
 package com.degloba.ecommerce.enviaments.infrastructure.persistence.nosql.mongo;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.query.MongoEntityInformation;
+import org.springframework.data.mongodb.repository.support.SimpleReactiveMongoRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.FluentQuery.ReactiveFluentQuery;
 import org.springframework.stereotype.Repository;
 
 import com.degloba.ecommerce.enviaments.domain.entitats.Enviament;
 import com.degloba.ecommerce.enviaments.domain.persistence.nosql.mongo.IEnviamentReactiveRepository;
 import com.degloba.infrastructure.services.GenericRepositoryImpl;
+import com.mongodb.client.result.DeleteResult;
 
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 
-
+@Slf4j
 @Repository
-public class EnviamentReactiveRepository extends GenericRepositoryImpl<Enviament, String> implements IEnviamentReactiveRepository {
-	
+public class EnviamentReactiveRepository extends SimpleReactiveMongoRepository<Enviament, String>
+		implements IEnviamentReactiveRepository {
 
+	public EnviamentReactiveRepository(@NonNull MongoEntityInformation<Enviament, String> entityInformation,
+			@NonNull ReactiveMongoOperations mongoOperations) {
+		super(entityInformation, mongoOperations);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Flux<Enviament> findAllByIdNotNullOrderByIdAsc(Pageable page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
