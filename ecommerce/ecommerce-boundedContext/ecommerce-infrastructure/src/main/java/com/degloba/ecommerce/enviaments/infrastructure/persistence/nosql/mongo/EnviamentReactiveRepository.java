7ca3712fcfae357486acbe3e1d3ee2047ecdbe37
degloba <degloba@degloba.com>
2021-12-23 17:06:26 +0100

    r

--- ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/enviaments/infrastructure/persistence/nosql/mongo/EnviamentReactiveRepository.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-infrastructure/src/main/java/com/degloba/ecommerce/enviaments/infrastructure/persistence/nosql/mongo/EnviamentReactiveRepository.java
@@ -7,19 +7,23 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import javax.annotation.Resource;
+
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.query.MongoEntityInformation;
-import org.springframework.data.mongodb.repository.support.SimpleReactiveMongoRepository;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.FluentQuery.ReactiveFluentQuery;
 import org.springframework.stereotype.Repository;
 
 import com.degloba.ecommerce.enviaments.domain.entitats.Enviament;
@@ -34,16 +38,32 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 
+import org.springframework.data.mongodb.repository.support.*;
+
+
+
 @Slf4j
 @Repository
 public class EnviamentReactiveRepository extends SimpleReactiveMongoRepository<Enviament, String>
-		implements IEnviamentReactiveRepository {
+	implements IEnviamentReactiveRepository {	
 
-
-	public EnviamentReactiveRepository(@NonNull MongoEntityInformation<Enviament, String> entityInformation,
-			@NonNull ReactiveMongoOperations mongoOperations) {
+	 
+	public EnviamentReactiveRepository(@NonNull MongoEntityInformation<Enviament, String> entityInformation, @NonNull ReactiveMongoOperations mongoOperations) {
 		super(entityInformation, mongoOperations);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Mono<Void> deleteAllById(Iterable<? extends String> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Enviament, R, P extends Publisher<R>> P findBy(Example<S> example,
+			Function<ReactiveFluentQuery<S>, P> queryFunction) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -51,5 +71,6 @@ public class EnviamentReactiveRepository extends SimpleReactiveMongoRepository<E
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 
 }
