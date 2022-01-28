61723cee6fbc45a51ddfbc263aa0be958d609ce0
degloba <degloba@degloba.com>
2022-01-28 19:54:00 +0100

    r

--- ecommerce/ecommerce-boundedContext/ecommerce-domain/src/main/java/com/degloba/ecommerce/enviaments/domain/persistence/nosql/mongo/IEnviamentReactiveRepository.java
+++ ecommerce/ecommerce-boundedContext/ecommerce-domain/src/main/java/com/degloba/ecommerce/enviaments/domain/persistence/nosql/mongo/IEnviamentReactiveRepository.java
@@ -1,19 +1,12 @@
 package com.degloba.ecommerce.enviaments.domain.persistence.nosql.mongo;
 
-import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.degloba.ecommerce.enviaments.domain.entitats.Enviament;
 
 import reactor.core.publisher.Flux;
-
-import org.springframework.data.mongodb.repository.support.SimpleMongoRepository;
-import org.springframework.data.mongodb.repository.support.SimpleReactiveMongoRepository;
 
 
 /**
